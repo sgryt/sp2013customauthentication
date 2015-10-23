a1529a6c79f192c7ef9b9283b978e2265da2920d
Mikkel Christensen <christensen.mikkel@gmail.com>
2015-10-23 21:08:37 +0200

    Sample from MS Support

---
+++ LOGIN/CustomRedirect/CustomRedirect.aspx
@@ -0,0 +1,4 @@
+﻿<%@ Assembly Name="$SharePoint.Project.AssemblyFullName$" %>
+<%@ Import Namespace="Microsoft.SharePoint.ApplicationPages" %>
+<%@ Import Namespace="Microsoft.SharePoint" %>
+<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CustomRedirect.aspx.cs" Inherits="CustomRedirect.Layouts.CustomRedirect.CustomRedirect" DynamicMasterPageFile="~/_layouts/15/errorv15.master" %>
---
+++ LOGIN/CustomRedirect/CustomRedirect.aspx.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.SharePoint;
+using Microsoft.SharePoint.Administration;
+using Microsoft.SharePoint.IdentityModel.Pages;
+using System;
+using System.Diagnostics;
+
+namespace CustomRedirect.Layouts.CustomRedirect
+{
+    public partial class CustomRedirect : IdentityModelSignInPageBase
+    {
+        static string HomeRealm = "whr";
+        static string WAuth = "wauth";
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            try
+            {
+                var webApp = SPContext.Current.Site.WebApplication;
+                if (webApp == null) return;
+
+                // Get redirection URL of SAML trust in current zone of current web app
+                string redirectUrl = String.Empty;
+                SPIisSettings iisSettings = webApp.GetIisSettingsWithFallback(SPContext.Current.Site.Zone);
+                if (!iisSettings.UseTrustedClaimsAuthenticationProvider)
+                    return;
+
+                foreach (SPAuthenticationProvider prov in iisSettings.ClaimsAuthenticationProviders)
+                {
+                    if (prov.GetType() == typeof(Microsoft.SharePoint.Administration.SPTrustedAuthenticationProvider))
+                    {
+                        redirectUrl = prov.AuthenticationRedirectionUrl.ToString();
+                    }
+                }
+
+                // Get all original query string parameters.
+                System.Text.StringBuilder additionalParameters = new System.Text.StringBuilder(2048);
+                additionalParameters.Append("&");
+                foreach (string key in this.Request.QueryString.Keys)
+                {
+                    additionalParameters.Append(key + "=" + Server.UrlEncode(this.Request.QueryString[key]) + "&");
+                }
+                additionalParameters.Append(HomeRealm + "=" + "testrealm" + "&");
+                additionalParameters.Append(WAuth + "=" + "urn:oasis:names:tc:SAML:1.0:am:password" + "&");
+
+                // Perform redirection
+                string fullUrl = redirectUrl + additionalParameters.ToString();
+                this.Response.Redirect(fullUrl);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+    }
+}
